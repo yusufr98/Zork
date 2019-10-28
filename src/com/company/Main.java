0a0b395ca21dc43742a29dbbbdb847c9ad3a674f
yusufr98 <yusuf090798@gmail.com>
2019-10-28 10:58:33 -0400

    moremehtods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,13 +11,61 @@ public class Main {
 
         System.out.println("Welcome to Zork! Type QUIT at any time to quit the game.");
 
-    }
-    public static void foyer() {
-        Scanner k = new Scanner(System.in);
-        System.out.println("You are in the foyer. You can choose which direction to move in next: ");
-        String d = k.next();
-        if (d.equalsIgnoreCase("north")) {
+        boolean repeat = true;
+        String direction;
+        int next = 1;
+        while(repeat){
+            if(next == 1){
+                next = foyer(direction);
+            }
+            else if(){
 
+            }
+        }
+    }
+    public static int foyer(String direction) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("You are in the foyer. There is a dead scorpion.");
+        System.out.println("Which direction do you want to go?");
+        direction = k.next();
+        if(direction.equalsIgnoreCase("n")){
+            return 2;
+        }
+        else if(direction.equalsIgnoreCase("quit")){
+            return -2;
+        }
+        else{
+            return -1;
+        }
+    }
+    public static int frontroom(String direction) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("You are in the front room. There is a piano.");
+        System.out.println("Which direction do you want to go?");
+        direction = k.next();
+        if(direction.equalsIgnoreCase("n")){
+            return 2;
+        }
+        else if(direction.equalsIgnoreCase("quit")){
+            return -2;
+        }
+        else{
+            return -1;
+        }
+    }
+    public static int foyer(String direction) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("You are in the foyer. There is a dead scorpion.");
+        System.out.println("Which direction do you want to go?");
+        direction = k.next();
+        if(direction.equalsIgnoreCase("n")){
+            return 2;
+        }
+        else if(direction.equalsIgnoreCase("quit")){
+            return -2;
+        }
+        else{
+            return -1;
         }
     }
 }
