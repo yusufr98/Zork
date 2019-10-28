38a81337e6d23fdf6e8d38364d111b1e4954feea
yusufr98 <yusuf090798@gmail.com>
2019-10-28 09:58:34 -0400

    second

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("Welcome to Zork! Type QUIT at any time to quit the game.");
 
+    }
+    public static int foyer(String direction) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("You are in the foyer. You can choose which direction to move in next: ");
+        String d = k.next();
+        if(direction.equalsIgnoreCase("north")){
+            return 2;
+        }
+        else{
+            return -1;
+        }
     }
 }
