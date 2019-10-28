412ebb9049cd16806206130af27efd30f804672a
tfranks247 <tfranks247@gmail.com>
2019-10-28 09:59:58 -0400

    i added some direction to the beginning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner k = new Scanner(System.in);
+        System.out.println("This is a directional adventure game. The goal is to find the secret room. " +
+                "From your starting room in the foyer you can move in any direction using" + "N,S, E, or W." + "Any other key will cause an error, good luck!");
 
     }
 }
